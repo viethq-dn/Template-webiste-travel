463277fcf829044975862fd47d34a09934f47951
Vi-cms <quocviet.php@gmail.com>
2019-10-24 21:45:15 +0700

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 import '../scss/index.scss';
+
+// var nav = $('.nav');
+// var xs = 768;
+// var windown = $(windown).width();
+
+function addIconMenu() {
+  $('.nav__list--child').each(function(i, obj) {
+    console.log(obj);
+    $(obj)
+      .parent()
+      .append('<i class="icon"><span></span></i>')
+      .children()
+      .on('click', function() {
+        $('.nav__list--child').removeClass('active');
+        $(this)
+          .prev()
+          .addClass('active');
+      });
+  });
+}
+
+$(function() {
+  addIconMenu();
+});
